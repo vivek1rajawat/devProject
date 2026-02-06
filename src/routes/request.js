8d7f73ace881cbd706618ffef7e6f2ed79a15fea
Vivek Singh Rajawat <vivekrajawat2005@gmail.com>
2026-02-06 15:26:23 +0530

    review api

--- src/routes/request.js
+++ src/routes/request.js
@@ -56,5 +56,42 @@ requestRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)
     }
 });
 
+requestRouter.post("/request/review/:status/:requestId", userAuth, async (req,res)=>{
+    try{
+        const loggedInUser = req.user;
+        const {status, requestId} = req.params;
+
+        const allowedStatus = ["accepted","rejected"];
+        if(!allowedStatus.includes(status)){
+            return res.status(400).json({message: "Status not allowed!" })
+        }
+
+        const connectionRequest = await ConnectionRequest.findOne({
+            _id: requestId,
+            toUserId: loggedInUser._id,
+            status: "interested",
+        });
+
+        if(!connectionRequest){
+            return res.status(404).json({message: "connection request not found"})
+        }
+
+        connectionRequest.status = status;
+
+        const data = await connectionRequest.save();
+        res.json({message: "Connection request " + status,data})
+        
+
+
+
+ 
+
+    } catch(err) {
+        res.status(404).send("ERROR " + err.message);
+
+    }
+
+
+})
 
 module.exports = requestRouter
